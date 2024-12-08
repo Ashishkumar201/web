f58c499d3d9806bedab455d2b7cdd62af23a015f
Ashishkumar201 <ashishkumarbagria@gmail.com>
2024-12-08 18:56:02 +0530

    maleobj file update

--- level0/maleobj.js
+++ level0/maleobj.js
@@ -1,3 +1,14 @@
-const people={
-    "Ashish" "male" "21" 
+let object ={}
+
+while(true)
+{
+    let key=("Enter the value of key or (q to quit")
+    if(key==='q')
+    { break;}
+
+
+let value=prompt("Enter the value for " +key + " +")
+object[key]=value
 }
+
+console.log(object)
